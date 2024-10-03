ea4b927b6efa1a07ecc90c74e1dfcb145e1ef832
dagwan1 <dan21017@byui.edu>
2024-10-03 01:53:27 +0100

    making changes

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -4,7 +4,11 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search() {
+interface SearchProps {
+  placeholder?: string; // Define placeholder as an optional prop
+}
+
+export default function Search({ placeholder = "Search..." }: SearchProps) { // Set a default value
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -21,6 +25,23 @@ export default function Search() {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+// export default function Search() {
+//   const searchParams = useSearchParams();
+//   const pathname = usePathname();
+//   const { replace } = useRouter();
+ 
+//   const handleSearch = useDebouncedCallback((term) => {
+//     console.log(`Searching... ${term}`);
+   
+//     const params = new URLSearchParams(searchParams);
+//     params.set('page', '1');
+//     if (term) {
+//       params.set('query', term);
+//     } else {
+//       params.delete('query');
+//     }
+//     replace(`${pathname}?${params.toString()}`);
+//   }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
